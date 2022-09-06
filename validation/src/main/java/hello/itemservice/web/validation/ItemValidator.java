29ef8de62e124e21ba542dfba067cb6a360a7c10
oni <rainbow2774@naver.com>
2022-09-06 15:27:46 +0900

    검증과정 BindingResult 활용과 그 구조 이해, 오류 코드를 코드를 변경하지 않고 properties를 사용하는법, 검증기 도입과 애노테이션으로 코드 단축
    Bean Validation 소개

---
+++ validation/src/main/java/hello/itemservice/web/validation/ItemValidator.java
@@ -0,0 +1,45 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.domain.item.Item;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.Validator;
+
+@Component
+public class ItemValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Item.class.isAssignableFrom(clazz);
+        // item == clazz
+        // item == subItem 자식 클래스여도 통과한다.
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Item item = (Item) target;
+
+        // errors는 bindingresult의 부모클래스여서 매개변수에 넣어줄 수가 있었다. 다형성때문에! errors의 자식이 result니까 부모는 자식을 담을 수 있다.
+
+        if (!StringUtils.hasText(item.getItemName())) {
+            errors.rejectValue("itemName", "required");
+        }
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            errors.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
+        }
+
+        if(item.getQuantity() == null || item.getQuantity() >= 9999) {
+            errors.rejectValue("quantity", "max", new Object[]{9999}, null);
+        }
+
+        if(item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000) {
+                errors.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+    }
+}
--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,9 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -24,6 +27,13 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
+
+    @InitBinder
+    public void init(WebDataBinder dataBinder) {
+        dataBinder.addValidators(itemValidator);
+    }
+    // 이 컨트롤러에서만 검증 가능
 
     @GetMapping
     public String items(Model model) {
@@ -46,7 +56,7 @@ public class ValidationItemControllerV2 {
     }
 
 //    @PostMapping("/add")
-    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         // 검증 오류 결과를 보관
 //        Map<String, String> errors = new HashMap<>();
@@ -61,7 +71,6 @@ public class ValidationItemControllerV2 {
 //            errors.put("price", "가격은 1,000원 ~ 1,000,000원 까지 허용합니다.");
             bindingResult.addError(new FieldError("item", "price", "가격은 1,000원 ~ 1,000,000원 까지 허용합니다."));
         }
-
 
         if(item.getQuantity() == null || item.getQuantity() >= 9999) {
 //            errors.put("quantity", "수량은 최대 9,999개 까지 허용합니다.");
@@ -93,7 +102,7 @@ public class ValidationItemControllerV2 {
     }
 
 //    @PostMapping("/add")
-    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         // 검증 오류 결과를 보관
 //        Map<String, String> errors = new HashMap<>();
@@ -110,7 +119,6 @@ public class ValidationItemControllerV2 {
 //            bindingResult.addError(new FieldError("item", "price", "가격은 1,000원 ~ 1,000,000원 까지 허용합니다."));
             bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null,  "가격은 1,000원 ~ 1,000,000원 까지 허용합니다."));
         }
-
 
         if(item.getQuantity() == null || item.getQuantity() >= 9999) {
 //            errors.put("quantity", "수량은 최대 9,999개 까지 허용합니다.");
@@ -142,8 +150,11 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
-    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        log.info("objectName={}", bindingResult.getObjectName());
+        log.info("target={}", bindingResult.getTarget());
 
         // 검증 로직
         if (!StringUtils.hasText(item.getItemName())) { // hasText() : 여기 itemName에 글자가 있냐 없냐?!
@@ -153,7 +164,6 @@ public class ValidationItemControllerV2 {
         if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
             bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000, 1000000}, null));
         }
-
 
         if(item.getQuantity() == null || item.getQuantity() >= 9999) {
             bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, new String[]{"max.item.quantity"}, new Object[]{9999}, null));
@@ -166,6 +176,89 @@ public class ValidationItemControllerV2 {
                 bindingResult.addError(new ObjectError("item",new String[]{"totalPriceMin"}, new Object[]{10000, resultPrice} , null));
             }
         }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm"; // 다시 입력폼으로 보낸다. 왜? 잘못된점을 고지하고 기존에 입력된값은 유지
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId()); // 기존에 입력했던값이 왜 계속 남아있는것이야??
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        if(bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        log.info("objectName={}", bindingResult.getObjectName());
+        log.info("target={}", bindingResult.getTarget());
+
+//        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required"); 아래와 동일
+
+        // 검증 로직
+        if (!StringUtils.hasText(item.getItemName())) { // hasText() : 여기 itemName에 글자가 있냐 없냐?!
+            bindingResult.rejectValue("itemName", "required");
+//            new String[]{"required.item.itemName", "required"};
+        }
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
+        }
+
+        if(item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000) {
+//                bindingResult.addError(new ObjectError("item",new String[]{"totalPriceMin"}, new Object[]{10000, resultPrice} , null));
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm"; // 다시 입력폼으로 보낸다. 왜? 잘못된점을 고지하고 기존에 입력된값은 유지
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId()); // 기존에 입력했던값이 왜 계속 남아있는것이야??
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        itemValidator.validate(item, bindingResult);
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm"; // 다시 입력폼으로 보낸다. 왜? 잘못된점을 고지하고 기존에 입력된값은 유지
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId()); // 기존에 입력했던값이 왜 계속 남아있는것이야??
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         // 검증에 실패하면 다시 입력 폼으로
         if(bindingResult.hasErrors()) {
