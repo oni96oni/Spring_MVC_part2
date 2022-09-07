e758f95b38d760168bc8966140138cb5f8ecafbf
oni <rainbow2774@naver.com>
2022-09-07 12:01:33 +0900

    Bean Validation 작동 과정 및 사용방법 에러코드적용순서 이해

---
+++ validation/src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -0,0 +1,34 @@
+package hello.itemservice.validation;
+
+import hello.itemservice.domain.item.Item;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+public class BeanValidationTest {
+    
+    @Test
+    void beanValidation() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        Item item = new Item();
+        item.setItemName(" ");
+        item.setPrice(0);
+        item.setQuantity(10000);
+
+        Set<ConstraintViolation<Item>> validations = validator.validate(item);
+        for (ConstraintViolation<Item> validation : validations) {
+            System.out.println("validation = " + validation);
+            System.out.println("object = " + validation.getRootBean());
+            System.out.println("invalidValue = " + validation.getInvalidValue());
+            System.out.println("message = " + validation.getMessage());
+            System.out.println("propertyPath = " + validation.getPropertyPath());
+            System.out.println("===========");
+        }
+    }
+}
